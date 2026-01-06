3996afe55074195823255cd2c459e9e075215c18
Mohammed Nassar <msh.nassar@gmail.com>
2026-01-06 16:16:48 +0000

    Add enum for data extraction task progress.

---
+++ src/DataExtraction/TaskProgress.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cordis\DataExtraction;
+
+/**
+ * Defines the task progress states for extraction.
+ */
+enum TaskProgress: string {
+
+  case Ongoing = 'ongoing';
+  case Finished = 'finished';
+  case Failed = 'failed';
+  case Deleted = 'deleted';
+
+}
