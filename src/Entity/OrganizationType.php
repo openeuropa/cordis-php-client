8993b0c8168c49b5709186ccd6ee1c0b210319c6
Mohammed Nassar <msh.nassar@gmail.com>
2026-01-06 15:27:50 +0000

    Add enum organisation types.

---
+++ src/Entity/OrganizationType.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cordis\Entity;
+
+/**
+ * Defines the organization types.
+ */
+enum OrganizationType: string {
+
+  case Coordinator = 'coordinator';
+  case Participant = 'participant';
+  case ThirdParty = 'thirdParty';
+  case AssociatedPartner = 'associatedPartner';
+
+}
