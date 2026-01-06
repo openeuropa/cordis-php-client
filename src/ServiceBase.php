a48d174f467b89cf40df4c3578f0352ddead8fb4
Mohammed Nassar <msh.nassar@gmail.com>
2026-01-06 16:10:49 +0000

    Add base class for a cordis service.

---
+++ src/ServiceBase.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cordis;
+
+use Cordis\Serializer\Serializer;
+use GuzzleHttp\ClientInterface as GuzzleClientInterface;
+
+/**
+ * Base class for cordis services.
+ */
+abstract class ServiceBase {
+
+  /**
+   * Service base constructor.
+   */
+  public function __construct(
+    protected GuzzleClientInterface $client,
+    protected $serializer = new Serializer(),
+  ) {
+  }
+
+}
