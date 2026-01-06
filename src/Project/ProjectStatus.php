553f6f42670d6d0485852bd08039360521566786
Mohammed Nassar <msh.nassar@gmail.com>
2026-01-06 16:12:44 +0000

    Add enum for project status.

---
+++ src/Project/ProjectStatus.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cordis\Project;
+
+/**
+ * Defines the project Statuses.
+ */
+enum ProjectStatus: string {
+  case Signed = 'signed';
+  case Ongoing = 'ongoing';
+  case Closed = 'closed';
+  case Terminated = 'terminated';
+
+}
