f5278aca39071666318f69c4a4dea476280a0b9c
Mohammed Nassar <msh.nassar@gmail.com>
2026-01-06 15:21:29 +0000

    Add class that represents an associated call.

---
+++ src/Entity/Call.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cordis\Entity;
+
+use Cordis\Entity\Elements\TitleTrait;
+use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\Type;
+
+/**
+ * Represents a call.
+ */
+class Call extends EntityBase implements AssociatedEntityInterface {
+
+  use AssociatedEntityTrait;
+  use TitleTrait;
+
+  #[Type("string")]
+  #[SerializedName("identifier")]
+  private ?string $identifier = NULL;
+
+  /**
+   * Get call identifier.
+   *
+   * @return string|null
+   *   The call identifier.
+   */
+  public function getIdentifier(): ?string {
+    return $this->identifier;
+  }
+
+}
