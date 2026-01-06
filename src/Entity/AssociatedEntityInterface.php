26fe17a84a75e2cbc8826de279d5183df0781fab
Mohammed Nassar <msh.nassar@gmail.com>
2026-01-06 15:10:21 +0000

    Add trait for associated entity.

---
+++ src/Entity/AssociatedEntityInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cordis\Entity;
+
+/**
+ * Interface for an associated entity.
+ *
+ * The interface should be implemented by the type of entities that could be
+ * associated.
+ * An associated entity is an entity that is associated with another entity.
+ * When an entity is associated, it also contains two attributes; type and
+ * source.
+ */
+interface AssociatedEntityInterface {
+
+  /**
+   * Get type.
+   *
+   * @return string|null
+   *   The type.
+   */
+  public function getType(): ?string;
+
+  /**
+   * Get source.
+   *
+   * @return string|null
+   *   The source.
+   */
+  public function getSource(): ?string;
+
+}
---
+++ src/Entity/AssociatedEntityTrait.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cordis\Entity;
+
+use Cordis\Entity\Attributes\SourceTrait;
+use Cordis\Entity\Attributes\TypeTrait;
+
+/**
+ * Trait for associated entity.
+ */
+trait AssociatedEntityTrait {
+
+  use TypeTrait;
+  use SourceTrait;
+
+}
