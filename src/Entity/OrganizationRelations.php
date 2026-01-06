39bd08e59d0de9a68d396181396cced144185c02
Mohammed Nassar <msh.nassar@gmail.com>
2026-01-06 15:26:28 +0000

    Add class for relations in an organisation associated entity.

---
+++ src/Entity/OrganizationRelations.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cordis\Entity;
+
+use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\Type;
+use JMS\Serializer\Annotation\XmlList;
+
+/**
+ * Represents relations in organization.
+ */
+class OrganizationRelations extends RelationsBase {
+
+  #[Type("array<Cordis\Entity\Region>")]
+  #[SerializedName("regions")]
+  #[XmlList(entry: "region")]
+  private array $regions = [];
+
+  /**
+   * Get regions in relations.
+   *
+   * @return \Cordis\Entity\Region[]
+   *   The regions.
+   */
+  public function getRegions(): array {
+    return $this->regions;
+  }
+
+}
